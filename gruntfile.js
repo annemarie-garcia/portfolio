2ceb3c9ea9e76749264fb18664043ca5d41136f5
Anne-Marie Garcia <ammieg@gmail.com>
2017-05-25 18:33:13 +0100

    Restructuring of folders and adding Grunt.

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-responsive-images');
+
+    grunt.initConfig({
+        concat: {
+            dist: {
+                src: [
+                    'src/js/jquery-3.2.1.min.js',
+                    'src/js/bootstrap.min.js',
+                    'src/js/scripts.js'  // This specific file
+                ],
+                dest: 'dist/js/production.js',
+            }
+        },
+        responsive_images: {
+                dev: {
+                    options: {
+                        sizes: [{
+                            width: 400
+                        },{
+                            width: 800
+                        }]
+                    },
+                    files: [{
+                        expand: true,
+                        src: ['**/*.{gif,jpeg,jpg,png}'],
+                        cwd:'src/images/',
+                        dest: 'dist/images/'
+                    }]
+                }
+        }
+   });
+
+    //orders it to execute the task.
+    grunt.registerTask('default', [
+        'concat',
+        'responsive_images'
+    ]);
+}
